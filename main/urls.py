447ab4a2fa675d93fa4e1bae1bab0b2f945373ce
Andreas Deturck <andreas.deturck@gmail.com>
2019-11-21 17:10:11 +0100

    Basic layout

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.counter, name='counter')
+    path('', views.counter, name='counter'),
+    path('increase_counter/<int:jaar>/<int:sprong>/', views.increase_counter, name='increase_counter')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-
+from django.db.models import F
 from .models import Jaar
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 
 def counter(request):
     jaren = Jaar.objects.order_by('jaar')
     context = {'jaren': jaren}
     return render(request, 'main/counter.html', context)
+
+
+@csrf_exempt
+def increase_counter(request, jaar, sprong):
+    _response = Jaar.objects.filter(jaar=jaar).update(aantal_punten=F('aantal_punten') + sprong)
+    return JsonResponse
